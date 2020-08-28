17e2b2fb15de56c79a85a1c9479562edcdbc55cb
codetyphon <oncwnuNcpkVqPNXxw_z6tJhS5ASE@git.weixin.qq.com>
2020-08-28 13:23:28 +0800

    demo

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useRef } from 'react'
+import { useObjects } from "use-tensorflow"
+import useCamera from "use-camera"
 import './App.css';
-
+function Box({left,top,width,height,label,color,score}){
+  return(
+  <div style={{
+    position:"absolute",
+    width:`${width}px`,
+    height:`${height}px`,
+    top:`${top}px`,
+    left:`${left}px`,
+    border:`solid 2px ${color}`,
+    borderRadius:'5px'
+  }}>{label}:{score.toFixed(2)}</div>
+  )
+}
 function App() {
+  // const ref = useRef(null);
+  const ref = useCamera({ audio: false });
+  // const objects = useObjects(ref);
+  const objects = useObjects(ref, { modelUrl: "/objects/model.json" });
+  let i = 0
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div style={{
+          position:"relative"
+        }}>
+          <video ref={ref} autoPlay width="640" height="480" />
+          {/* <img ref={ref} src="/living-room.jpg" /> */}
+          {objects ? objects.map(({ left, top, width, height, label, score }) => {
+            i+=1
+            return (
+          <Box
+            key={i}
+            left={left}
+            top={top}
+            width={width}
+            height={height}
+            label={label}
+            color={score > 0.6 ? "blue" : "red"}
+            score={score}
+          />
+          )
+          }) : 'Loading...'}
+        </div>
       </header>
     </div>
   );
